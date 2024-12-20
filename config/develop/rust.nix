7f984abd0911dc3837f1057f9d1edf531971b1e3
Cedrik Hoffmann <choffmann@progeek.de>
2024-12-20 15:52:17 +0100

    feat: add rust definitions

---
+++ config/develop/rust.nix
@@ -0,0 +1,13 @@
+{
+  plugins.lsp.servers = {
+    rust_analyzer = {
+      enable = true;
+      installCargo = true;
+      installRustc = true;
+    };
+  };
+
+  plugins.lint.lintersByFt = {
+      rust = [ "clippy" ];
+  };
+}
